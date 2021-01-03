8b356d950762f07a69db00e3d0825b868d9e389a
Tumi <oluwatumininuojo25@gmail.com>
2021-01-03 12:43:19 +0100

    kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,19 +9,21 @@ fun main() {
     println(a*b)
 
     // The popular hello world message
-    
+
     println("Hello World")
 
+    // Basic arithmetic operations
 
     println(22/7)
     println(22.0 / 7)
     println(20.0 % 3.2)
-    println("Hi there")
 
     // So shl is a way of multiplying using 2 and shr is a 
     // way of diving by 2
 
     println(1 shl 3) // This is the same as 1 x 2^3
     println(32 shr 2) // This is the same as 32 / 2^2
+
+    
 
 }
