3ed5211df3e87f47f58e7eb0e262b8d9deae7111
Tumi <oluwatumininuojo25@gmail.com>
2021-01-03 12:39:11 +0100

    kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
+// Do you know that comments in Kotlin can be nested?
+
 fun main() {
     val a = 4
     val b = 3
     println(a*b)
     println("Hello World")
+    println(22/7)
+    println(22.0 / 7)
+    println(20.0 % 3.2)
+    println("Hi there")
+
+    // So shl is a way of multiplying using 2 and shr is a 
+    // way of diving by 2
+
+    println(1 shl 3) // 
+    println(32 shr 2)
 }
