cbe747a6e3ba0e1847e5b6cded73b2acd37e6c92
Tumi <oluwatumininuojo25@gmail.com>
2021-01-03 15:09:34 +0100

    kt

--- src/main/kotlin/Strings.kt
+++ src/main/kotlin/Strings.kt
@@ -19,4 +19,6 @@ fun Main () {
     println("How are you doing $name")
 
     //You can also use expressions in string template
+    val oneThirdString = "One third is ${1.0/3.0}"
+    println(oneThirdString)
 }
