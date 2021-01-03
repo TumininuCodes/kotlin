14d1dc754699c0d8c8c0e5824841a3129371b21b
Tumi <oluwatumininuojo25@gmail.com>
2021-01-03 12:41:36 +0100

    kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 // Do you know that comments in Kotlin can be nested?
 
 fun main() {
+
+    // val is like value and var is like variable
+
     val a = 4
     val b = 3
     println(a*b)
     println("Hello World")
+
+
     println(22/7)
     println(22.0 / 7)
     println(20.0 % 3.2)
@@ -14,5 +19,6 @@ fun main() {
     // way of diving by 2
 
     println(1 shl 3) // This is the same as 1 x 2^3
-    println(32 shr 2)
+    println(32 shr 2) // This is the same as 32 / 2^2
+
 }
