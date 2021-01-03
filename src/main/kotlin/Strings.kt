f27b3edabf70ceeb5f66636e99e61a024f6ade40
Tumi <oluwatumininuojo25@gmail.com>
2021-01-03 14:39:52 +0100

    kt

--- src/main/kotlin/Strings.kt
+++ src/main/kotlin/Strings.kt
@@ -1,10 +1,12 @@
 fun Main () {
 
-    // An
-    
+    // An example of concatenation of strings
+
     var newChar: Char = '!'
     var greetingMessage = "Hello Tumininu"
-    greetingMessage += newChar
+    greetingMessage += newChar // This is one way
+
+    var concatenatedString = greetingMessage + newChar
 
     println(greetingMessage)
 }
