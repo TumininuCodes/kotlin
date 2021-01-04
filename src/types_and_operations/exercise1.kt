278825e99dced7bdbefd14f411c87f5312e82d06
Tumi <oluwatumininuojo25@gmail.com>
2021-01-04 16:01:38 +0100

    kotlin

--- src/types_and_operations/exercise1.kt
+++ src/types_and_operations/exercise1.kt
@@ -12,4 +12,12 @@ fun main() {
     val celcius = (dF - 32) / 1.8
     println("Celsius temperature is $celcius")
 
+    val bigString =
+        """
+                Hi! My name is Tumininu Ojo. I am a software developer and I love solving problems
+                Yeah, so there is nothing much or big to say about me... yet
+                So yeah, that's it. We have tested Kotlin's multiline string feature
+            """.trimIndent()
+
+    println(bigString)
 }
