7086bc7a495c0ef89beb0b379bb36dec65b86b99
Tumi <oluwatumininuojo25@gmail.com>
2021-01-04 16:27:35 +0100

    kotlin

--- src/types_and_operations/Pairs_Triples.kt
+++ src/types_and_operations/Pairs_Triples.kt
@@ -5,8 +5,17 @@ fun main() {
     val cordinatesInt: Pair<Int, Int> = Pair(2, 3)
     val cordinatesDouble = Pair(2.1, 3.1)
     val cordinatesWithTo = 2 to 3
+    val cordinatesMixed = Pair(2.1, 3)
 
     println(cordinatesInt)
     println(cordinatesDouble)
     println(cordinatesWithTo)
+    println(cordinatesMixed)
+
+    // And here is how to access the data
+    val x1 = cordinatesInt.first
+    val y1 = cordinatesInt.second
+
+    println(x1)
+    println(y1)
 }
