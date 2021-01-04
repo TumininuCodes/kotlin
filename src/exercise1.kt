9fd01c3233b1b61031178025feb35599cbe1fbcd
Tumi <oluwatumininuojo25@gmail.com>
2021-01-04 15:38:25 +0100

    kotlin

--- src/exercise1.kt
+++ src/exercise1.kt
@@ -1,28 +1,10 @@
-/* 1. Declare two constants a and b of type Double and assign both a value. Calculate
-    the average of a and b and store the result in a constant named average.*/
-
-/* 2. A temperature expressed in °C can be converted to °F by multiplying by 1.8
-    then incrementing by 32. In this challenge, do the reverse: convert a temperature
-    from °F to °C. Declare a constant named fahrenheit of type Double and assign it a value.
-    Calculate the corresponding temperature in °C and store the result in a constant named celcius.*/
-
-
-/* 3. Suppose the squares on a chessboard are numbered left to right, top to bottom,
-    with 0 being the top-left square and 63 being the bottom-right square.
-    Rows are numbered top to bottom, 0 to 7. Columns are numbered left to right, 0 to 7.*/
-
-
-/* 4. Declare a constant position and assign it a value between 0 and 63.
-    Calculate the corresponding row and column numbers and store the results in constants
-    named row and column. 8. A circle is made up of 2𝜋 radians, corresponding with 360 degrees.
-    Declare a constant degrees of type Double and assign it an initial value. Calculate the corresponding
-    angle in radians and store the result in a constant named radians.*/
-
-/* 5. Declare four constants named x1, y1, x2 and y2 of type Double. These constants represent the two-dimensional
-    coordinates of two points. Calculate the distance between these two points and store the result
-    in a constant named distance*/
+const val a = 1.0
+const val b = 2.0
 
 fun main() {
+
+    val average = (a + b) / 2
+    print(average)
 
 
 }
