7626e1cd349ed944e543255c5bcf3640c3a487f5
Tumi <oluwatumininuojo25@gmail.com>
2021-01-04 15:23:57 +0100

    kotlin

--- src/exercise1.kt
+++ src/exercise1.kt
@@ -1,1 +1,7 @@
-//
+// 1.  Declare two constants a and b of type Double and assign both a value. Calculate
+// the average of a and b and store the result in a constant named average.
+
+
+fun main() {
+
+}
