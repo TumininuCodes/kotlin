91eea089427a5bf91f80523bc136cf29011b9796
Tumi <oluwatumininuojo25@gmail.com>
2021-01-04 16:22:07 +0100

    kotlin

--- src/types_and_operations/Pairs_Triples.kt
+++ src/types_and_operations/Pairs_Triples.kt
@@ -1,2 +1,12 @@
 package types_and_operations
 
+fun main() {
+
+    val cordinatesInt: Pair<Int, Int> = Pair(2, 3)
+    val cordinatesDouble = Pair(2.1, 3.1)
+    val cordinatesWithTo = 2 to 3
+
+    println(cordinatesInt)
+    println(cordinatesDouble)
+    println(cordinatesWithTo)
+}
