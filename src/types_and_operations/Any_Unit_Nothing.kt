a3cc8fc66de04a3556f4f26853d7e4c62d409da6
Tumi <oluwatumininuojo25@gmail.com>
2021-01-05 08:34:44 +0100

    kotlin

--- src/types_and_operations/Any_Unit_Nothing.kt
+++ src/types_and_operations/Any_Unit_Nothing.kt
@@ -6,4 +6,21 @@ fun main() {
 
     println(anyNumber)
     println(anyString)
+
+    add()
+}
+
+// This function returns a Unit type. Similar to void in Java
+
+fun add(): Unit {
+    val result = 2 + 2
+    println(result)
+}
+
+// Lol, don't run this, it is the land of nothing
+
+fun doNothingForever(): Nothing {
+    while (true) {
+
+    }
 }
