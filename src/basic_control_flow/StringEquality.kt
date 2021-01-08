8e454b4dce85f893e637f29b394a4ebd89e51180
Tumi <oluwatumininuojo25@gmail.com>
2021-01-08 03:22:11 +0100

    kotlin

--- src/basic_control_flow/StringEquality.kt
+++ src/basic_control_flow/StringEquality.kt
@@ -4,7 +4,14 @@ fun main() {
 
     val guess = "dog"
 
+    // Comparison using equal to
     val dogEqualCat = guess == "cat"
 
     println(dogEqualCat)
+
+    // Greater than or lesser than operator are evaluated alphabetically
+    val order = ("cat" < "dog")
+    println(order)
+
+    // This means higher alphabets are greater than lower alphabets
 }
