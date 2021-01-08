c5b8b2e05c0b1d06ca9ecee78ec6263a46afdbde
Tumi <oluwatumininuojo25@gmail.com>
2021-01-08 08:57:06 +0100

    kotlin

--- src/basic_control_flow/DoWhile.kt
+++ src/basic_control_flow/DoWhile.kt
@@ -1,2 +1,18 @@
 package basic_control_flow
 
+fun main() {
+    var sum = 1
+
+    do {
+        sum += 2
+        println(sum)
+    } while (sum < 3)
+
+    while (true) {
+        sum += 3
+        println(sum)
+        if (sum >= 200) {
+            break
+        }
+    }
+}
