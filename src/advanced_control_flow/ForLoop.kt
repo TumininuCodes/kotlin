07aede4c5c65df472a78b1e522602693ba3f1e55
Tumi <oluwatumininuojo25@gmail.com>
2021-01-08 09:19:01 +0100

    kotlin

--- src/advanced_control_flow/ForLoop.kt
+++ src/advanced_control_flow/ForLoop.kt
@@ -1,2 +1,30 @@
 package advanced_control_flow
 
+fun main() {
+
+    val count = 10
+    var sum = 0
+
+    for (i in 1..count) {
+        sum += i
+        println(sum)
+    }
+
+    sum = 0
+
+    for (i in count downTo 1 step 2) {
+        sum += i
+        println(sum)
+    }
+
+    sum = 1
+    var lastSum = 0
+    repeat(10) {
+        val temp = sum
+        sum += lastSum
+        lastSum = temp
+        println(lastSum)
+    }
+
+
+}
--- src/advanced_control_flow/Range.kt
+++ src/advanced_control_flow/Range.kt
@@ -1,2 +1,12 @@
 package advanced_control_flow
 
+fun main() {
+    val closedRange = 0..5
+    println(closedRange)
+
+    val halfOpenRange = 0 until 5
+    println(halfOpenRange)
+
+    val decreasingRange = 5 downTo 0
+    println(decreasingRange)
+}
