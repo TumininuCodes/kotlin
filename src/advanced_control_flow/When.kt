2bebd3352077109486979e1e0546d29602e6d217
Tumi <oluwatumininuojo25@gmail.com>
2021-01-10 02:51:02 +0100

    kotlin

--- src/advanced_control_flow/When.kt
+++ src/advanced_control_flow/When.kt
@@ -32,4 +32,22 @@ fun main() {
     }
 
     println(numberName)
+
+
+    val hourOfDay = 12
+    val timeOfDay: String
+
+    timeOfDay = when (hourOfDay) {
+        0, 1, 2, 3, 4, 5 -> "Early morning"
+        6, 7, 8, 9, 10, 11 -> "Morning"
+        12, 13, 14, 15, 16 -> "Afternoon"
+        17, 18, 19 -> "Evening"
+        20, 21, 22, 23 -> "Late evening"
+
+        else -> "INVALID HOUR!"
+    }
+
+    println(timeOfDay)
+
+    // Ranges can also be used with when. For example in 0..4
 }
