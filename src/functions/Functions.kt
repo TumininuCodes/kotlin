8d091a6c73b145b1cc554c9e033fb5cd0ba9e714
Tumi <oluwatumininuojo25@gmail.com>
2021-01-10 03:34:05 +0100

    kotlin

--- src/functions/Functions.kt
+++ src/functions/Functions.kt
@@ -2,8 +2,14 @@ package functions
 
 fun main() {
     printMultipleOfFive(10)
+
+    val (product, quotient) = multiplyAndDivide(4, 2)
 }
 
 fun printMultipleOfFive(value: Int) {
     println("$value * 5 = ${value * 5}")
+}
+
+fun multiplyAndDivide(number: Int, factor: Int): Pair<Int, Int> {
+    return Pair(number * factor, number / factor)
 }
