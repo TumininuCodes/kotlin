be59838c6cecc5da04c39f678be0651f1be2cf03
Tumi <oluwatumininuojo25@gmail.com>
2021-01-11 05:36:44 +0100

    kotlin

--- src/functions/Functions.kt
+++ src/functions/Functions.kt
@@ -1,11 +1,17 @@
 package functions
 
+import types_and_operations.add
+
 fun main() {
     printMultipleOfFive(10)
 
     val (product, quotient) = multiplyAndDivide(4, 2)
     println(product)
     println(quotient)
+
+    var usingReferenceOperator = ::add
+
+    println(usingReferenceOperator)
 }
 
 fun printMultipleOfFive(value: Int) {
@@ -14,4 +20,8 @@ fun printMultipleOfFive(value: Int) {
 
 fun multiplyAndDivide(number: Int, factor: Int): Pair<Int, Int> {
     return Pair(number * factor, number / factor)
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
 }
