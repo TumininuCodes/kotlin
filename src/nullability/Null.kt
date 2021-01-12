a9036e7a479223a064cccdee9c22d8f5a3e64aa9
Tumi <oluwatumininuojo25@gmail.com>
2021-01-12 06:26:59 +0100

    kotlin

---
+++ src/nullability/Null.kt
@@ -0,0 +1,12 @@
+package nullability
+
+fun main() {
+    // You declare a variable of a nullable type by using the following syntax
+    var errorCode: Int?
+
+    errorCode = 100
+    errorCode = null
+
+    val result: Int? = 30
+    println(result)
+}
