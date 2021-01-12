c840b6cad49f15707506a7f7fa48fbf9fa279aae
Tumi <oluwatumininuojo25@gmail.com>
2021-01-12 07:37:28 +0100

    kotlin

--- src/nullability/Null.kt
+++ src/nullability/Null.kt
@@ -9,4 +9,10 @@ fun main() {
 
     val result: Int? = 30
     println(result)
+
+    var authorName: String? = "Joe Howard"
+    var authorAge: Int? = 24
+
+    val ageAfterBirthday = authorAge!! + 1
+    println("After his next birthday, author's age will be $ageAfterBirthday")
 }
