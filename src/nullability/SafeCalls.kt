d05cd9bd65ec73bf4e510ece7df02d4754aad20f
Tumi <oluwatumininuojo25@gmail.com>
2021-01-14 07:45:30 +0100

    kotlin

--- src/nullability/SafeCalls.kt
+++ src/nullability/SafeCalls.kt
@@ -3,7 +3,9 @@ package nullability
 fun main() {
     val authorName: String? = "Joe Howard"
 
-    var nameLength = authorName?.length
-
+    val nameLength = authorName?.length
     println("Author's name has length $nameLength")
+
+    val nameLength5 = authorName?.length?.plus(5)
+    println("Author's name has length $nameLength5")
 }
