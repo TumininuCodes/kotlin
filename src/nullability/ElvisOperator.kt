e31e84e9411323774c8540fcdf8ba08f3e7e4571
Tumi <oluwatumininuojo25@gmail.com>
2021-01-14 08:10:24 +0100

    kotlin

--- src/nullability/ElvisOperator.kt
+++ src/nullability/ElvisOperator.kt
@@ -10,4 +10,9 @@ fun main() {
     /* Using the Elvis operator means mustHaveResult will equal either the value inside nullableInt, or 0 if
      nullableInt contains null. In this example, mustHaveResult is inferred to be of type Int and contains the
      concrete Int value of 10 */
+
+    nullableInt = null
+
+    mustHaveResult = nullableInt ?: 0
+    println(mustHaveResult)
 }
