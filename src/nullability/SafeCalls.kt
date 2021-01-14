9692ec3422f711235cd1d01f74b1f58cab66dd23
Tumi <oluwatumininuojo25@gmail.com>
2021-01-14 07:55:00 +0100

    kotlin

--- src/nullability/SafeCalls.kt
+++ src/nullability/SafeCalls.kt
@@ -2,10 +2,21 @@ package nullability
 
 fun main() {
     val authorName: String? = "Joe Howard"
+    var nonNullableAuthor: String
 
-    val nameLength = authorName?.length
+    var nameLength = authorName?.length
     println("Author's name has length $nameLength")
 
     val nameLength5 = authorName?.length?.plus(5)
     println("Author's name has length $nameLength5")
+
+    // Using safe call with the let() function
+    authorName?.let {
+        nonNullableAuthor = authorName
+        nameLength = authorName.length
+
+        println(nonNullableAuthor)
+        println(nameLength.toString())
+    }
+
 }
