0d97619483c90aa5c216871aa44664b15182244d
Tumi <oluwatumininuojo25@gmail.com>
2021-01-23 11:45:10 +0100

    kotlin

---
+++ src/arrays_and_lists/Lists.kt
@@ -0,0 +1,7 @@
+package arrays_and_lists
+
+fun main() {
+    val innerPlanets = listOf("Mercury", "Venus", "Earth", "Saturn")
+    // The above can also be written as listOf<String>(...)
+    println(innerPlanets)
+}
