5090846276f2df9e751ecdd8ac11b7d03041ba81
Tumi <oluwatumininuojo25@gmail.com>
2021-01-30 13:26:55 +0100

    kotlin

--- src/maps_and_sets/Sets.kt
+++ src/maps_and_sets/Sets.kt
@@ -1,5 +1,15 @@
 package maps_and_sets
 
 fun main() {
+    val names = setOf("Anna", "Brian", "Craig", "Anna")
+    println(names)
 
+    // You can also use hashSetOf()
+
+    // Adding elements
+    val someSets = mutableSetOf(1, 2, 3, 4)
+    someSets.add(5)
+    println(someSets)
+    someSets.remove(5)
+    println(someSets)
 }
