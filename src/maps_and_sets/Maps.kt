7fa0d57b57615adf299745a5e9a14b2a5690a72a
Tumi <oluwatumininuojo25@gmail.com>
2021-01-30 12:59:10 +0100

    kotlin

--- src/maps_and_sets/Maps.kt
+++ src/maps_and_sets/Maps.kt
@@ -1,11 +1,25 @@
 package maps_and_sets
 
 fun main() {
-    var yearOfBirth = mapOf("Anna" to 1990, "Tumi" to 2001, "Jide" to 1999)
+    val yearOfBirth = mapOf("Anna" to 1990, "Tumi" to 2001, "Jide" to 1999)
 
-    var namesAndScores = mutableMapOf("Anna" to 2, "Brian" to 2, "Craig" to 8, "Donna" to 6)
+    val namesAndScores = mutableMapOf("Anna" to 2, "Brian" to 2, "Craig" to 8, "Donna" to 6)
     println(namesAndScores)
 
-    var pairs = hashMapOf("Anna" to 1990, "Tumi" to 2001, "Jide" to 1999)
+    val pairs = hashMapOf("Anna" to 1990, "Tumi" to 2001, "Jide" to 1999)
     println(pairs)
+
+    // Adding pairs to maps
+    val bobData = mutableMapOf("name" to "Bob", "profession" to "CardPlayer", "country" to "USA")
+    bobData.put("state", "CA")
+    // there are other ways this can be done though
+
+
+    // Updating values
+    bobData.put("name", "Bobby")
+
+    // Removing pairs
+    bobData.remove("country")
+
+    println(bobData)
 }
